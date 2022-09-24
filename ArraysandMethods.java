aaf8dd7a075284bf620d0ef4990f18766b622744
Christopher Needham <christopher.needham09@gmail.com>
2022-09-24 18:01:48 -0500

    first commit

---
+++ ArraysandMethods.java
@@ -0,0 +1,21 @@
+package com.christopherneedham.week34;
+
+
+public class ArraysandMethods{
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		int[] ages = new int[8];
+		ages[0] = 3;
+		ages[1] = 9;
+		ages[2] = 23;
+		ages[3] = 64;
+		ages[4] = 2;
+		ages[5] = 8;
+		ages[6] = 28;
+		ages[7] = 93;
+		
+			
+	}
+		
+}
